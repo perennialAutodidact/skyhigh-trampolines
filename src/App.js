4c19a48b2557d4c7130e7410ee9c5906fcab56d9
keegangood <keegood8@gmail.com>
2022-09-13 11:22:10 -0700

    switched css files to scss files and included bootstrap classnames to test setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Counter from './components/Counter'
 import logo from './logo.svg'
-import './App.css'
+import './App.scss'
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="bg-primary text-secondary">
+          Edit <code className='text-info'>src/App.js</code> and save to reload.
         </p>
         <Counter />
       </header>
