498e6631fef299b4dc35d1b0e4df34908beab24c
Monique McIntyre <skaiwilliams85@gmail.com>
2022-09-13 16:12:42 -0500

    Installed missing dependencies

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import Counter from './components/Counter'
-import logo from './logo.svg'
-import './App.scss'
+import Counter from "./components/Counter";
+import logo from "./logo.svg";
+import "./App.scss";
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p className="bg-primary text-secondary">
-          Edit <code className='text-info'>src/App.js</code> and save to reload.
+          Edit <code className="text-info">src/App.js</code> and save to reload.
         </p>
         <Counter />
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
