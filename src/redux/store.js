aa158b8f2a456086324a23027aa0747b1fe7e688
keegangood <keegood8@gmail.com>
2022-09-15 14:35:59 -0700

    added roomSlice reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './authSlice'
 import counterReducer from './counterSlice'
 import usersReducer from './usersSlice'
+import roomsReducer from './roomsSlice'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     auth: authReducer,
     users: usersReducer,
+    rooms: roomsReducer
   },
 })
