1bf759dd791c8a258506aef95500e863954f1fa7
Jin Choo <jinchoo315@gmail.com>
2022-09-14 21:44:46 -0500

    connect pages to the router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import Counter from './components/Counter'
-import logo from './logo.svg'
 import './App.scss'
+import AddOnForm from './components/AddOnForm'
+import ProductForm from './components/ProductForm'
+import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <div>
+        Home page
+        <Link to="addon">Add on Page</Link>
+        <Link to="productForm">Product Form</Link>
+      </div>
+    ),
+  },
+  {
+    path: '/addon',
+    element: <AddOnForm headerText="Add on" />,
+  },
+  {
+    path: '/productForm',
+    element: <ProductForm headerText="Product Form" />,
+  },
+])
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p className="bg-primary text-secondary">
-          Edit <code className='text-info'>src/App.js</code> and save to reload.
-        </p>
-        <Counter />
+    <div className="App">
+      <header className="App-header">
+        {/* <Counter /> */}
+        {/* <ProductForm /> */}
       </header>
+      <RouterProvider router={router} />
     </div>
   )
 }
