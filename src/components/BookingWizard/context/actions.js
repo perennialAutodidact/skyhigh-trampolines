8b9b1d189e51e48d4a7ad2c9d472367bb1f71c6e
keegangood <keegood8@gmail.com>
2022-09-16 12:54:32 -0700

    added setProgressBarStep action

--- src/components/BookingWizard/context/actions.js
+++ src/components/BookingWizard/context/actions.js
@@ -2,3 +2,10 @@ export const updateForm = (partialFormData) => ({
   type: 'UPDATE_FORM',
   payload: partialFormData
 })
+
+export const setProgressBarStep = (step) => ({
+  type: 'UPDATE_PROGRESS_BAR',
+  payload: {
+    step
+  }
+})
