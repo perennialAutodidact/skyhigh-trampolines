7bc3715a27f0838e3c5365bdc8bbd72e06118823
keegangood <keegood8@gmail.com>
2022-09-16 12:53:56 -0700

    added intial formData

--- src/components/BookingWizard/context/index.js
+++ src/components/BookingWizard/context/index.js
@@ -3,7 +3,11 @@ import { createContext } from "react";
 export const initialState = {
   percentComplete: 0,
   currentStep: 1,
-  totalSteps: 6
+  totalSteps: 6,
+  formData: {
+    date: '',
+    time: ''
+  }
 }
 
 export const BookingWizardContext = createContext(initialState)
