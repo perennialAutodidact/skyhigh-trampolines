16104d20392d274b20cd296b5592477988c69eca
keegangood <keegood8@gmail.com>
2022-09-16 10:38:37 -0700

    added state values

--- src/components/BookingWizard/context/index.js
+++ src/components/BookingWizard/context/index.js
@@ -1,5 +1,9 @@
 import { createContext } from "react";
 
-export const initialState = {}
+export const initialState = {
+  percentComplete: 0,
+  currentStep: 1,
+  totalSteps: 6
+}
 
 export const BookingWizardContext = createContext(initialState)
