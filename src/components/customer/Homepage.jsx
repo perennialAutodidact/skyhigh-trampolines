ab1e09d6c411d652ab612822c1988c1216f582ef
keegangood <keegood8@gmail.com>
2022-09-16 14:59:06 -0700

    added link to buy tickets

--- src/components/customer/Homepage.jsx
+++ src/components/customer/Homepage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "./Homepage.module.scss";
 
 const Homepage = () => {
   return (
-    <section className={styled.homepage}>
-      <figure className="h-100 w-100">
+    <section className={`${styled.homepage} position-relative`}>
+      <figure className="h-100 w-100 position-relative">
         <img
           className="img-fluid h-100 w-100"
           src="https://images.unsplash.com/photo-1612985838143-47ffc9b76532?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1175&q=80"
@@ -13,10 +14,12 @@ const Homepage = () => {
       </figure>
 
       <div
-        className={`${styled.overlay} position-absolute d-flex justify-content-center align-items-center w-100  bg-overlay bg-opacity-50`}
+        className={`${styled.overlay} position-absolute d-flex justify-content-center align-items-center w-100 bg-overlay bg-opacity-50`}
       >
-        <button type="button" className="btn btn-light btn-lg rounded-0  ">
-          Buy Tickets
+        <button type="button" className="btn btn-light btn-lg rounded shadow">
+          <Link to="/booking" className="text-decoration-none link-dark fs-5 fw-bold">
+            Buy Tickets
+          </Link>
         </button>
       </div>
     </section>
