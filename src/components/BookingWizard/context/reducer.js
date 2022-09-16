5a3abd8e2a2fb05c0a35b2a2eaed90484d507e0a
keegangood <keegood8@gmail.com>
2022-09-16 10:39:06 -0700

    added UPDATE_PROGRESS_BAR reducer

--- src/components/BookingWizard/context/reducer.js
+++ src/components/BookingWizard/context/reducer.js
@@ -1,10 +1,23 @@
+const formatPercent = (x, y) => Math.round(x / y) * 100;
+
 export const wizardReducer = (state, action) => {
   switch (action.type) {
-    case 'UPDATE_FORM':
+    case "UPDATE_FORM":
       return {
         ...state,
-        ...action.payload
-      }
+        formData: {
+          ...state.formData,
+          ...action.payload,
+        },
+      };
+    case "UPDATE_PROGRESS_BAR":
+      const { step } = action.payload;
+      const { totalSteps } = state;
+      return {
+        ...state,
+        currentStep: step,
+        percentComplete: formatPercent(step, totalSteps),
+      };
     default:
       return state;
   }
