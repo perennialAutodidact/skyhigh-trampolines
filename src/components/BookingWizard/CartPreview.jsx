a4c219c340377c7e3d6e80a6078523eb46d3bb02
keegangood <keegood8@gmail.com>
2022-09-16 10:39:23 -0700

    created cart preview component

--- src/components/BookingWizard/CartPreview.jsx
+++ src/components/BookingWizard/CartPreview.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const CartPreview = () => {
   return (
-    <div>CartPreview</div>
-  )
-}
+    <div className="container mt-3 pt-3 border border-grey rounded">
+      <h3>Cart</h3>
+    </div>
+  );
+};
 
-export default CartPreview
+export default CartPreview;
