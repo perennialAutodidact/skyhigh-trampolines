11b480ff0a6224cf526642fe919e62a6c2dc0093
keegangood <keegood8@gmail.com>
2022-09-16 10:38:02 -0700

    added bookingSchema and step schemas

---
+++ src/components/BookingWizard/schema.js
@@ -0,0 +1,9 @@
+import { object, string } from "yup";
+
+export const bookingSchema = object().shape({});
+export const step1Schema = object().shape({});
+export const step2Schema = object().shape({});
+export const step3Schema = object().shape({});
+export const step4Schema = object().shape({});
+export const step5Schema = object().shape({});
+export const step6Schema = object().shape({});
