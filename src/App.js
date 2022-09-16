ce22e2868766773752d193c3b6ec6b65c489c4e2
Waldo Lavaut <waldothedeveloper@gmail.com>
2022-09-16 11:57:22 -0400

    Added Admin, Customer, ProtectedRoute routes, and redirect non-auth users back to Login route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import Counter from './components/Counter'
-import logo from './logo.svg'
-import './App.scss'
+import "./App.scss";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { Admin } from "./components/Admin";
+import { Customer } from "./components/Customer";
+import Login from "./components/login/Login";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import React from "react";
+import { auth } from "./firebase/client";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+//
 
 function App() {
+  const [user, loading] = useAuthState(auth);
+
+  //
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p className="bg-primary text-secondary">
-          Edit <code className='text-info'>src/App.js</code> and save to reload.
-        </p>
-        <Counter />
-      </header>
-    </div>
-  )
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute isAllowed={!!user} loading={loading}>
+                <Admin />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/" element={<Customer />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
 }
 
-export default App
+export default App;
--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -1,31 +1,35 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-	user: '',
-	email: '',
-	accessToken: '',
-}
+  user: "",
+  email: "",
+  accessToken: "",
+  role: "admin",
+};
 
 export const authSlice = createSlice({
-	name: 'auth',
-	initialState,
-	reducers: {
-		login: (state, action) => {
-			state.user = action.payload.displayName
-			state.email = action.payload.email
-			state.accessToken = action.payload.accessToken
-			console.log(state.user)
-		},
-		logout: (state) => {
-			state.user = ''
-			state.email = ''
-			state.accessToken = ''
-			console.log(state.user)
-		},
-	},
-})
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload.displayName;
+      state.email = action.payload.email;
+      state.accessToken = action.payload.accessToken;
+      console.log(
+        `User authenticated? ${state.user} and role is ${state.role}`
+      );
+    },
+    logout: (state) => {
+      state.user = "";
+      state.email = "";
+      state.accessToken = "";
+
+      console.log(`user logged out`, state.user);
+    },
+  },
+});
 
 // Action creators are generated for each case reducer function
-export const { login, logout } = authSlice.actions
+export const { login, logout } = authSlice.actions;
 
-export default authSlice.reducer
+export default authSlice.reducer;
