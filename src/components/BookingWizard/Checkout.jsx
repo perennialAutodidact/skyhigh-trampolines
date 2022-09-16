2a08b6530a7142672703e39378c486625366b2d5
keegangood <keegood8@gmail.com>
2022-09-16 13:54:40 -0700

    added basic layout styling

--- src/components/BookingWizard/Checkout.jsx
+++ src/components/BookingWizard/Checkout.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { BookingWizardContext } from "./context";
+import { setProgressBarStep } from "./context/actions";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { step6Schema } from "./schema";
 
 const Checkout = () => {
-  return (
-    <div>Checkout</div>
-  )
-}
+  const navigate = useNavigate();
+  const [state, dispatch] = useContext(BookingWizardContext);
 
-export default Checkout
+  const initialValues = {
+  };
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm({
+    initialValues,
+    resolver: yupResolver(step6Schema),
+  });
+
+  const onSubmit = (formData) => {
+    console.log(state.formData)
+  };
+
+  const goBack = () => dispatch(setProgressBarStep(5));
+
+  return (
+    <div className="container pt-3">
+      <h3>Checkout</h3>
+      <form onSubmit={handleSubmit(onSubmit)} className="container text-start">
+        <div className="row my-3 align-items-end">
+          <div className="col-12 col-lg-2 p-0">
+            <Link
+              to="/booking/step-5"
+              onClick={goBack}
+              className="link-dark text-decoration-none"
+            >
+              Back
+            </Link>
+          </div>
+          <div className="col col-12 col-lg-4 offset-lg-6 p-0">
+            <button type="submit" className="btn btn-success w-100">
+              Checkout
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Checkout;
