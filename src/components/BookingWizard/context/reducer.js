9058abc46ce7f9e1fc2361f757239c2781021da3
keegangood <keegood8@gmail.com>
2022-09-16 08:19:46 -0700

    created reducer for 'UPDATE_FORM'

--- src/components/BookingWizard/context/reducer.js
+++ src/components/BookingWizard/context/reducer.js
@@ -1,5 +1,10 @@
 export const wizardReducer = (state, action) => {
   switch (action.type) {
+    case 'UPDATE_FORM':
+      return {
+        ...state,
+        ...action.payload
+      }
     default:
       return state;
   }
