36c1ecc5fa46883c9fdcd2c6b18cd579ea9da6b7
Jin Choo <jinchoo315@gmail.com>
2022-09-16 15:26:16 -0500

    add route to the AddOnForm

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import "./App.scss";
+import './App.scss'
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import { Admin } from "./components/Admin";
-import { Customer } from "./components/Customer";
-import Login from "./components/login/Login";
-import { ProtectedRoute } from "./components/ProtectedRoute";
-import React, {useState} from "react";
-import { auth } from "./firebase/client";
-import { useAuthState } from "react-firebase-hooks/auth";
-
+import { Admin } from './components/Admin'
+import { Customer } from './components/Customer'
+import Login from './components/login/Login'
+import { ProtectedRoute } from './components/ProtectedRoute'
+import React, { useState } from 'react'
+import { auth } from './firebase/client'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import AddOnForm from './components/AddOnForm'
 //
 
 function App() {
-  const [user, loading] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth)
   // state to toggle sidebar in admin
-  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const [toggleSidebar, setToggleSidebar] = useState(false)
   //
   return (
     <React.StrictMode>
@@ -26,15 +26,17 @@ function App() {
             element={
               <ProtectedRoute isAllowed={!!user} loading={loading}>
                 <Admin />
+                <AddOnForm />
               </ProtectedRoute>
             }
           />
+
           <Route path="/" element={<Customer />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </React.StrictMode>
-  );
+  )
 }
 
-export default App;
+export default App
