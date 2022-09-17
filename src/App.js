1d925fedfe8d24041b4a10f75f28f82b1360962a
Jin Choo <jinchoo315@gmail.com>
2022-09-16 20:28:37 -0500

    add onSumit button

--- src/App.js
+++ src/App.js
@@ -10,13 +10,16 @@ import React, { useState } from 'react'
 import { auth } from './firebase/client'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import AddOnForm from './components/AddOnForm'
+import { useDispatch } from 'react-redux'
 //
-
+import { createAddOns } from './redux/addOnsSlice'
 function App() {
   const [user, loading] = useAuthState(auth)
   // state to toggle sidebar in admin
   const [toggleSidebar, setToggleSidebar] = useState(false)
   //
+
+  const dispatch = useDispatch()
   return (
     <React.StrictMode>
       <BrowserRouter>
@@ -26,11 +29,21 @@ function App() {
             element={
               <ProtectedRoute isAllowed={!!user} loading={loading}>
                 <Admin />
-                <AddOnForm />
               </ProtectedRoute>
             }
           />
-
+          <Route
+            path="/addon"
+            element={
+              <AddOnForm
+                headerText={'Add On'}
+                onSubmit={(e) => {
+                  console.log(e)
+                  dispatch(createAddOns(e))
+                }}
+              />
+            }
+          />
           <Route path="/" element={<Customer />} />
           <Route path="/login" element={<Login />} />
         </Routes>
