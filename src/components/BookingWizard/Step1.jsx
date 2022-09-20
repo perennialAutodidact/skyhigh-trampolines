a305999699d82cb26cd32cf8b79b4535ca6fce67
keegangood <keegood8@gmail.com>
2022-09-20 12:11:52 -0700

    removed effect and formatted

--- src/components/BookingWizard/Step1.jsx
+++ src/components/BookingWizard/Step1.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { BookingWizardContext } from "./context";
 import { updateForm, setProgressBarStep } from "./context/actions";
@@ -26,18 +26,12 @@ const Step1 = () => {
 
   const onSubmit = (formData) => {
     dispatch(updateForm(formData));
-    dispatch(setProgressBarStep(2))
-    navigate('/booking/step-2')
+    dispatch(setProgressBarStep(2));
+    navigate("/booking/step-2");
   };
-
-
-  useEffect(()=>{
-    setValue('date', state.formData.date)
-  }, [setValue, state])
 
   return (
     <div className="container pt-3">
-
       <form onSubmit={handleSubmit(onSubmit)} className="container text-start">
         {/* DATE */}
         <div className="row mb-3">
@@ -45,7 +39,7 @@ const Step1 = () => {
             <label htmlFor="date" className="form-label p-0 d-flex gap-1">
               <h3>Select Date</h3> <span className="text-danger">*</span>
             </label>
-            <CalendarDatePicker setFormValue={setValue}/>
+            <CalendarDatePicker setFormValue={setValue} />
             {errors.date && (
               <p className="text-danger">{errors.date.message}</p>
             )}
@@ -54,10 +48,7 @@ const Step1 = () => {
 
         <div className="row my-3 align-items-end">
           <div className="col-12 col-lg-2 p-0">
-            <Link
-              to="/"
-              className="link-dark text-decoration-none"
-            >
+            <Link to="/" className="link-dark text-decoration-none">
               Back
             </Link>
           </div>
@@ -68,7 +59,6 @@ const Step1 = () => {
           </div>
         </div>
       </form>
-  
     </div>
   );
 };
