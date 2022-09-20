66fb762a5d6377ce24be680294594a8f88ccdfb1
keegangood <keegood8@gmail.com>
2022-09-20 11:16:33 -0700

    created CalendarRow component

---
+++ src/components/BookingWizard/CalendarDatePicker/CalendarRow.jsx
@@ -0,0 +1,49 @@
+import React, { useContext } from "react";
+import { CalendarDatePickerContext } from "./context";
+import { dateIsSelectable, dateIsSelected } from "./utils";
+import { setSelectedDate } from "./context/actions";
+import styles from "./CalendarDatePicker.module.scss";
+
+const CalendarRow = ({ dates }) => {
+  const [state, dispatch] = useContext(CalendarDatePickerContext);
+  const { currentPage, selectedDate } = state;
+
+  const handleDateSelect = (date) => {
+    if (dateIsSelectable(currentPage.date, selectedDate, date)) {
+      dispatch(setSelectedDate(date));
+    }
+  };
+
+  return (
+    <tr className="row">
+      {dates.map((date) => (
+        <td
+          className="col p-0 py-1 py-md-2 d-flex justify-content-center align-items-center"
+          key={date.date()}
+        >
+          <div
+            onClick={() => handleDateSelect(date)}
+            className={`
+              ${styles.calendarDateNumber} 
+              rounded-circle
+              d-flex justify-content-center align-items-center 
+              ${
+                dateIsSelectable(currentPage.date, selectedDate, date)
+                  ? "fw-bold"
+                  : "text-muted"
+              }
+              ${
+                dateIsSelected(currentPage.date, selectedDate, date) &&
+                styles.calendarDateNumber__active
+              }
+              `}
+          >
+            {date.date()}
+          </div>
+        </td>
+      ))}
+    </tr>
+  );
+};
+
+export default CalendarRow;
