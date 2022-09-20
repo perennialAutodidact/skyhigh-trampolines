f9671a3518b57f7057473e5894ea8c372c11b9b4
keegangood <keegood8@gmail.com>
2022-09-20 09:26:04 -0700

    added MonthYearDisplay component

--- src/components/CalendarDatePicker/index.jsx
+++ src/components/CalendarDatePicker/index.jsx
@@ -1,14 +1,21 @@
 import React, { useReducer } from "react";
 import { CalendarDatePickerContext, initialState } from "./context";
 import { calendarDatePickerReducer } from "./context/reducer";
+import MonthYearDisplay from "./MonthYearDisplay";
 
 const CalenderDatePicker = () => {
   const [state, dispatch] = useReducer(calendarDatePickerReducer, initialState);
 
   return (
-    <CalendarDatePickerContext.Provider
-      value={[state, dispatch]}
-    ></CalendarDatePickerContext.Provider>
+    <CalendarDatePickerContext.Provider value={[state, dispatch]}>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12">
+            <MonthYearDisplay />
+          </div>
+        </div>
+      </div>
+    </CalendarDatePickerContext.Provider>
   );
 };
 
