ae2abe2fa421ee8ad2b3c7cc270fb3ad9f4256d2
keegangood <keegood8@gmail.com>
2022-09-20 11:15:36 -0700

    adjusted responsive column widths and padding

--- src/components/BookingWizard/CalendarDatePicker/MonthYearDisplay.jsx
+++ src/components/BookingWizard/CalendarDatePicker/MonthYearDisplay.jsx
@@ -17,7 +17,7 @@ const MonthYearDisplay = () => {
   const year = currentPage.date.format("YYYY");
 
   return (
-    <div className="col-4 offset-4 my-3 d-flex justify-content-between align-items-center gap-3">
+    <div className="col-12 col-md-4 offset-md-4 my-3 d-flex justify-content-between align-items-center gap-3">
       <GoArrowLeft onClick={goToPrevMonth} />
       <div>
         {month} {year}
