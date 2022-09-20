56961f4d7b085735f2b8c0015f951beb082dc21b
keegangood <keegood8@gmail.com>
2022-09-20 09:58:55 -0700

    cleaned up WeekdayLabels logic

--- src/components/BookingWizard/CalendarDatePicker/WeekdayLabels.jsx
+++ src/components/BookingWizard/CalendarDatePicker/WeekdayLabels.jsx
@@ -3,13 +3,15 @@ import dayjs from "dayjs";
 
 const WeekdayLabels = () => {
   const formatCode = "ddd";
-  const getDayLabel = (dayNumber, format) => dayjs().day(dayNumber).format(format);
+  const dayLabels = [...new Array(7)].map((_, dayNumber) =>
+    dayjs().day(dayNumber).format(formatCode)
+  ); // ['SUN', 'MON', 'TUE', ...]
 
   return (
     <tr className="row">
-      {[...new Array(7)].map((_, dayNumber) => (
-        <th className="col p-0 text-uppercase">
-          {getDayLabel(dayNumber, formatCode)}
+      {dayLabels.map((dayLabel) => (
+        <th className="col p-0 text-uppercase" key={dayLabel}>
+          {dayLabel}
         </th>
       ))}
     </tr>
