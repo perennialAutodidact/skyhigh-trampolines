5540cfeea04dcf8dc2a3a4bdeb1d72cddb597b1c
keegangood <keegood8@gmail.com>
2022-09-20 11:14:45 -0700

    set initial form rows

--- src/components/BookingWizard/CalendarDatePicker/context/index.js
+++ src/components/BookingWizard/CalendarDatePicker/context/index.js
@@ -1,10 +1,11 @@
 import { createContext } from "react";
 import dayjs from 'dayjs'
+import { getPageRowData } from "../utils";
 
 export const initialState = {
   currentPage: {
     date: dayjs(),
-    rows: []
+    rows: getPageRowData(dayjs())
   },
   selectedDate: dayjs(),
 }
