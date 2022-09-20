116157411a5ce5f80df3890ebc5c49dbd85e1ee6
keegangood <keegood8@gmail.com>
2022-09-20 09:33:02 -0700

    added reducer case for set current page date

--- src/components/BookingWizard/CalendarDatePicker/context/reducer.js
+++ src/components/BookingWizard/CalendarDatePicker/context/reducer.js
@@ -1,6 +1,12 @@
 export const calendarDatePickerReducer = (state, action) => {
   switch (action.type) {
     
+    case "SET_CURRENT_PAGE_DATE":
+      return {
+        ...state,
+        currentPageDate: action.payload.date
+      }
+
     default:
       return state;
   }
