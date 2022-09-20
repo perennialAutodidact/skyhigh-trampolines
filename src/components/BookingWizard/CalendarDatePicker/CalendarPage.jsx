bc57d67c6ecb3b6a705df460688fe6744ecbe522
keegangood <keegood8@gmail.com>
2022-09-20 12:10:52 -0700

    removed console log and formatted

--- src/components/BookingWizard/CalendarDatePicker/CalendarPage.jsx
+++ src/components/BookingWizard/CalendarDatePicker/CalendarPage.jsx
@@ -2,12 +2,16 @@ import React, { useContext } from "react";
 import { CalendarDatePickerContext } from "./context";
 import CalendarRow from "./CalendarRow";
 
-const CalendarPage = ({dates}) => {
-  const [state, dispatch] = useContext(CalendarDatePickerContext);
+const CalendarPage = ({ dates }) => {
+  const [state] = useContext(CalendarDatePickerContext);
   const { currentPage } = state;
-  console.log({currentPage})
 
-  return currentPage.rows && currentPage.rows.map((row,i) => <CalendarRow dates={row} key={`row-${i}`}/>);
+  return (
+    currentPage.rows &&
+    currentPage.rows.map((row, i) => (
+      <CalendarRow dates={row} key={`row-${i}`} />
+    ))
+  );
 };
 
 export default CalendarPage;
