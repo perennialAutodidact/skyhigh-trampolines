fdfaea011e4a5ddca4762c4e3e89113eafdbb93c
KG <keegood8@gmail.com>
2022-09-22 23:16:13 -0700

    rendered ProductSelect component

--- src/components/BookingWizard/Step2.jsx
+++ src/components/BookingWizard/Step2.jsx
@@ -5,6 +5,7 @@ import { updateForm, setProgressBarStep } from "./context/actions";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { step2Schema } from "./schema";
+import ProductSelect from "./ProductSelect";
 
 const Step2 = () => {
   const navigate = useNavigate();
@@ -35,31 +36,19 @@ const Step2 = () => {
   return (
     <div className="container pt-3">
       <form onSubmit={handleSubmit(onSubmit)} className="container text-start">
-        {/* TIME */}
+        {/* PRODUCT SELECT */}
         <div className="row mb-3">
           <div className="col-12 col-lg-6 p-0">
             <label htmlFor="date" className="form-label p-0 d-flex gap-1">
-              <h3>Select Arrival Time</h3>{" "}
-              <span className="text-danger">*</span>
+              <h3>Select Products</h3> <span className="text-danger">*</span>
             </label>
-            <input
-              {...register("time")}
-              id="time"
-              type="hidden"
-              className={`form-control ${errors.date && "is-invalid"}`}
-            />
-            {errors.date && (
-              <p className="text-danger">{errors.date.message}</p>
-            )}
+
+            {/* {errors.products && (
+              <p className="text-danger">{errors.products.message}</p>
+            )} */}
           </div>
         </div>
-        <div className="d-flex gap-2">
-          {ARRIVAL_TIMES.map((time) => (
-            <div className="border border-grey p-2" key={time}>
-              {time}
-            </div>
-          ))}
-        </div>
+        <ProductSelect />
 
         <div className="row my-3 align-items-end">
           <div className="col-12 col-lg-2 p-0 mb-3 mb-lg-0 order-2 order-lg-1">
