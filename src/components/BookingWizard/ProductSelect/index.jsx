0dd053260e6e37e517503c76c11d6a5d230f9d1f
keegangood <keegood8@gmail.com>
2022-09-23 14:48:57 -0700

    fixed accordion opening logic and rendered product list

--- src/components/BookingWizard/ProductSelect/index.jsx
+++ src/components/BookingWizard/ProductSelect/index.jsx
@@ -1,36 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { getHalfHourIncrementStrings } from "./utils";
+import styles from "../BookingWizard.module.scss";
+import ProductList from "./ProductList";
+
 const ProductSelect = ({ roomData }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
   const ARRIVAL_TIMES = getHalfHourIncrementStrings("9:00", "16:30");
 
+  const toggleIsExpanded = () => setIsExpanded((isExpanded) => !isExpanded);
+
   return (
-    <div className="accordion-item">
-      <h2 className="accordion-header" id="panelsStayOpen-headingOne">
-        <button
-          className="accordion-button"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target={`#panelsStayOpen-${roomData.id}`}
-          aria-expanded="true"
-          aria-controls={`panelsStayOpen-${roomData.id}`}
+    <div className="accordion accordion-flush" id="rooms-accordion">
+      <div className="accordion-item mb-2">
+        <h2 className="accordion-header" id={`accordion-header-${roomData.id}`}>
+          <div className="container-fluid p-0">
+            <button
+              className="accordion-button ps-0 py-0 row g-0 gap-2"
+              type="button"
+              onClick={toggleIsExpanded}
+              data-bs-toggle="collapse"
+              data-bs-target={`#accordion-collapse-${roomData.id}`}
+              aria-expanded={isExpanded}
+              aria-controls={`accordion-collapse-${roomData.id}`}
+            >
+              <div className="col-4 col-lg-3">
+                <img
+                  src={roomData.photo}
+                  alt={roomData.name + " Photo"}
+                  className={`${styles.accordionHeaderImage}`}
+                />
+              </div>
+
+              <div className="col-6 col-lg-7">
+                <h4 className="m-0">{roomData.name}</h4>
+              </div>
+            </button>
+          </div>
+        </h2>
+        <div
+          id={`accordion-collapse-${roomData.id}`}
+          className="accordion-collapse collapse show"
+          aria-labelledby={`accordion-header-${roomData.id}`}
         >
-          {roomData.name}
-        </button>
-      </h2>
-      <div
-        id={`room-${roomData.id}`}
-        className="accordion-collapse collapse show"
-        aria-labelledby="panelsStayOpen-headingOne"
-      >
-        <div className="accordion-body">
-          <strong>This is the first item's accordion body.</strong> It is shown
-          by default, until the collapse plugin adds the appropriate classes
-          that we use to style each element. These classes control the overall
-          appearance, as well as the showing and hiding via CSS transitions. You
-          can modify any of this with custom CSS or overriding our default
-          variables. It's also worth noting that just about any HTML can go
-          within the <code>.accordion-body</code>, though the transition does
-          limit overflow.
+          <div className="accordion-body px-0 container-fluid">
+            <ProductList products={roomData.products} />
+          </div>
         </div>
       </div>
     </div>
