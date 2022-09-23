a0da757f753eb59c14c9269d39aac4b426b761f3
Jin Choo <jinchoo315@gmail.com>
2022-09-23 13:51:44 -0500

    initializing firebase functions

--- src/firebase/client.js
+++ src/firebase/client.js
@@ -1,40 +1,42 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
-import "@firebase/auth";
-import "@firebase/storage";
-import "firebase/firestore";
+import { initializeApp } from 'firebase/app'
+import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+import { getStorage } from 'firebase/storage'
+import '@firebase/auth'
+import '@firebase/storage'
+import 'firebase/firestore'
+import { getFunctions } from 'firebase/functions'
 
-const firebaseKey = process.env.REACT_APP_FIREBASE_API_KEY;
-const firebaseProjectId = process.env.REACT_APP_FIREBASE_PROJECT_ID;
-const firebaseAuthDomain = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN;
-const firebaseStorageBucket = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET;
+const firebaseKey = process.env.REACT_APP_FIREBASE_API_KEY
+const firebaseProjectId = process.env.REACT_APP_FIREBASE_PROJECT_ID
+const firebaseAuthDomain = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN
+const firebaseStorageBucket = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
 
 const firebaseConfig = {
   apiKey: firebaseKey,
   authDomain: firebaseAuthDomain,
   projectId: firebaseProjectId,
   storageBucket: firebaseStorageBucket,
-};
+}
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
-const storage = getStorage(app);
-const colRef = collection(db, "Product Form");
-const productsRef = collection(db, "products");
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+const auth = getAuth(app)
+const storage = getStorage(app)
+const functions = getFunctions(app)
+const colRef = collection(db, 'Product Form')
+const productsRef = collection(db, 'products')
 
 getDocs(colRef)
   .then((snapshot) => {
-    let productForm = [];
+    let productForm = []
     snapshot.forEach((doc) => {
-      productForm.push({ ...doc.data(), id: doc.id });
-    });
-    console.log(productForm);
+      productForm.push({ ...doc.data(), id: doc.id })
+    })
+    console.log(productForm)
   })
   .catch((err) => {
-    console.log(err.message);
-  });
+    console.log(err.message)
+  })
 
-export { db, auth, storage, colRef, productsRef };
+export { db, auth, storage, functions, colRef, productsRef }
