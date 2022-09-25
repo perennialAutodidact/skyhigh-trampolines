e7a5fed2c67c297e7c655a66f1bd5a81df30d1cf
keegangood <keegood8@gmail.com>
2022-09-24 17:14:56 -0700

    pulled values from context

--- src/components/BookingWizard/RoomAccordion/index.jsx
+++ src/components/BookingWizard/RoomAccordion/index.jsx
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { getRoomList } from "../../../redux/roomsSlice";
-import styles from "../BookingWizard.module.scss";
+import React, { useState, useContext } from "react";
+import styles from "./RoomAccordion.module.scss";
+import { ProductSelectContext } from "../Step2/context";
 import ProductList from "./ProductList";
 import StartTimeList from "./StartTimeList";
 
-const RoomAccordion = ({ roomData }) => {
+const RoomAccordion = ({ roomIndex }) => {
+  const [state, dispatch] = useContext(ProductSelectContext);
+  const { rooms, startTimes } = state;
+
+  const room = rooms[roomIndex];
   const [isExpanded, setIsExpanded] = useState(false);
 
   const toggleIsExpanded = () => setIsExpanded((isExpanded) => !isExpanded);
@@ -15,7 +18,7 @@ const RoomAccordion = ({ roomData }) => {
       <div className="accordion-item mb-2">
         <h2
           className="accordion-header shadow border-0"
-          id={`accordion-header-${roomData.id}`}
+          id={`accordion-header-${room.id}`}
         >
           <div className="container-fluid p-0">
             <button
@@ -23,32 +26,36 @@ const RoomAccordion = ({ roomData }) => {
               type="button"
               onClick={toggleIsExpanded}
               data-bs-toggle="collapse"
-              data-bs-target={`#accordion-collapse-${roomData.id}`}
+              data-bs-target={`#accordion-collapse-${room.id}`}
               aria-expanded={isExpanded}
-              aria-controls={`accordion-collapse-${roomData.id}`}
+              aria-controls={`accordion-collapse-${room.id}`}
             >
               <div className="col-4 col-lg-3">
                 <img
-                  src={roomData.photo}
-                  alt={roomData.name + " Photo"}
+                  src={room.photo}
+                  alt={room.name + " Photo"}
                   className={`${styles.accordionHeaderImage}`}
                 />
               </div>
 
               <div className="col-6 col-lg-7">
-                <h4 className="m-0">{roomData.name}</h4>
+                <h4 className="m-0">{room.name}</h4>
               </div>
             </button>
           </div>
         </h2>
         <div
-          id={`accordion-collapse-${roomData.id}`}
+          id={`accordion-collapse-${room.id}`}
           className="accordion-collapse collapse show"
-          aria-labelledby={`accordion-header-${roomData.id}`}
+          aria-labelledby={`accordion-header-${room.id}`}
         >
-          <StartTimeList />
+          <StartTimeList
+            startTimes={startTimes}
+            selectedStartTime={room.selectedStartTime}
+            disabledStartTimes={room.disabledStartTimes}
+          />
           <div className="accordion-body container-fluid px-3">
-            <ProductList products={roomData.products} />
+            <ProductList products={room.products} />
           </div>
         </div>
       </div>
