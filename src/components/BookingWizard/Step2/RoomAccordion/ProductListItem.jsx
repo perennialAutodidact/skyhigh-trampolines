3361d8058e8b1454130f32ff2a399e6cebc5468e
KG <keegood8@gmail.com>
2022-09-25 07:48:50 -0700

    dispatched action to set product quantity

--- src/components/BookingWizard/Step2/RoomAccordion/ProductListItem.jsx
+++ src/components/BookingWizard/Step2/RoomAccordion/ProductListItem.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ProductSelectContext } from "../context";
+import { setProductQuantity } from "../context/actions";
 import { BsPlusLg, BsDashLg } from "react-icons/bs";
-const ProductListItem = ({ product }) => {
+
+const ProductListItem = ({ product, roomId }) => {
+  const [state, dispatch] = useContext(ProductSelectContext);
+  const onChange = (e) => {
+    dispatch(setProductQuantity(roomId, product.id, e.target.value));
+  };
+
   return (
     <div className="row g-0 gy-1">
       <div className="col-5 col-lg-4 offset-lg-2">{product.name}</div>
@@ -17,6 +25,8 @@ const ProductListItem = ({ product }) => {
             type="text"
             className="form-control p-0 text-center"
             aria-label={product.name}
+            value={product.quantity}
+            onChange={onChange}
           />
           <div className="btn btn-outline-secondary d-flex justify-content-center align-items-center p-1">
             <BsPlusLg />
