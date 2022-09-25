ddd4714bb58a78fac23cfddb2cb0aa445048cedd
keegangood <keegood8@gmail.com>
2022-09-24 17:15:32 -0700

    added conditional rendering classes

--- src/components/BookingWizard/RoomAccordion/StartTimeList.jsx
+++ src/components/BookingWizard/RoomAccordion/StartTimeList.jsx
@@ -1,15 +1,25 @@
-import React from "react";
-import { getHalfHourIncrementStrings } from "./utils";
+import styles from "../RoomAccordion/RoomAccordion.module.scss";
 
-const StartTimeList = () => {
-  const START_TIMES = getHalfHourIncrementStrings("9:00", "16:30");
+const StartTimeList = ({
+  startTimes,
+  selectedStartTime,
+  disabledStartTimes,
+}) => {
   return (
     <>
       <div className="text-center pt-3">Start time</div>
       <div className="row gy-1 px-5">
-        {START_TIMES.map((time) => (
+        {startTimes.map((time) => (
           <div className="col-3 col-lg-2 p-1" key={time}>
-            <div className="py-1 border text-center">{time}</div>
+            <div
+              className={`
+                py-1 border text-center
+                ${time === selectedStartTime ? "bg-info" : ""}
+                ${disabledStartTimes.includes(time) ? styles.disabledTime : ""}
+                `}
+            >
+              {time}
+            </div>
           </div>
         ))}
       </div>
