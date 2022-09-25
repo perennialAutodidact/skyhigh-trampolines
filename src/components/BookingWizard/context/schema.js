ab567de317160192b4e384a3ec9028f9b45a21d1
KG <keegood8@gmail.com>
2022-09-25 07:46:56 -0700

    added step2 schema

--- src/components/BookingWizard/context/schema.js
+++ src/components/BookingWizard/context/schema.js
@@ -8,11 +8,16 @@ export const step1Schema = object().shape({
   date: string().required("Please select a date."),
 });
 
-export const step2Schema = object().shape({});
+export const step2Schema = object().shape({
+  productsSelected: boolean().oneOf(
+    [true],
+    "Please select at least one product."
+  ),
+});
 export const step3Schema = object().shape({});
 export const step4Schema = object().shape({});
 export const step5Schema = object().shape({
   waiverAgreed: boolean().oneOf([true], "Waiver must be agreed to.").required(),
-  signatureImageData: string().required("Waiver must be signed.")
+  signatureImageData: string().required("Waiver must be signed."),
 });
 export const step6Schema = object().shape({});
