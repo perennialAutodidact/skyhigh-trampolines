5914aa7e60c427b4e402fdd9ade35bfc073c2ab4
KG <keegood8@gmail.com>
2022-09-27 07:23:26 -0700

    stripe intent works, but still returns error from cloud function

--- src/components/BookingWizard/TestPayment.jsx
+++ src/components/BookingWizard/TestPayment.jsx
@@ -4,18 +4,20 @@ import { httpsCallable } from "firebase/functions";
 
 const TestPayment = () => {
   const fetchPaymentSecret = async () => {
+    console.log(functions);
     try {
       const createPaymentIntent = httpsCallable(
         functions,
         "createPaymentIntent"
       );
+
       const response = await createPaymentIntent({
         amount: 199,
-        message: "test",
+        metadata: { order_id: "1234" },
       });
       console.log(response);
     } catch (error) {
-      console.log(error);
+      console.log("CREATE PAYMENT ERROR", error);
     }
   };
 
