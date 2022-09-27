e4b3e3c45f087a04095e4c04d360961abb0f24ca
keegangood <keegood8@gmail.com>
2022-09-27 09:31:45 -0700

    returned client secret from function and removed log statement in error block

--- functions/payments.js
+++ functions/payments.js
@@ -4,15 +4,17 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 exports.createPaymentIntent = functions.https.onCall(async (data, context) => {
   const { amount, metadata } = data;
   try {
-    return await stripe.paymentIntents.create({
+    const paymentIntent = await stripe.paymentIntents.create({
       amount,
       currency: "usd",
       automatic_payment_methods: { enabled: true },
       metadata,
     });
+
+    const {client_secret} = paymentIntent
+
+    return {client_secret}
   } catch (error) {
-    functions.logger.log("Stripe Payment Intent Error:", error);
-    // console.log(error);
     throw new functions.https.HttpsError("unknown", error);
   }
 });
