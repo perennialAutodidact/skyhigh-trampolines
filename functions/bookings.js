4c9a11569475e58c513a02771d6fb62a04cc71f2
keegangood <keegood8@gmail.com>
2022-09-29 16:28:58 -0700

    setup creating booking in db

--- functions/bookings.js
+++ functions/bookings.js
@@ -1,5 +1,12 @@
-const functions = require('firebase-functions')
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-exports.createBooking = functions.https.onCall((data,context)=>{
-  functions.logger.log("CREATE BOOKING DATA", data)
-})
+admin.initializeApp(functions.config().firebase)
+const db = admin.firestore()
+
+exports.createBooking = functions.https.onCall((data, context) => {
+  functions.logger.log("CREATE BOOKING DATA", data);
+  db.collection("bookings").doc().set({
+    
+  })
+});
