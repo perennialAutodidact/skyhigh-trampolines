b5e7f025b604cc6c717257f7a0dde2be93a69d74
KG <keegood8@gmail.com>
2022-09-29 11:00:17 -0700

    added thunk to get fake addon data

--- src/redux/addOnsSlice.js
+++ src/redux/addOnsSlice.js
@@ -22,6 +22,31 @@ export const createAddOn = createAsyncThunk(
   }
 );
 
+export const getAddOnsList = createAsyncThunk(
+  "addOns/getAddOnsList",
+  async (_, { fulfillWithValue, rejectWithValue }) => {
+    try {
+      const addOns = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      ).then((res) => TEMP_ADD_ONS);
+
+      return fulfillWithValue(addOns);
+    } catch (error) {
+      rejectWithValue("error");
+    }
+  },
+  {
+    condition: (_, { getState, extra }) => {
+      const { addOns } = getState();
+      const fetchStatus = addOns.loading;
+      if (fetchStatus === "fulfilled" || fetchStatus === "pending") {
+        // Already fetched or in progress, don't need to re-fetch
+        return false;
+      }
+    },
+  }
+);
+
 const addOnsSlice = createSlice({
   name: "addOns",
   initialState: {
@@ -39,7 +64,44 @@ const addOnsSlice = createSlice({
     [createAddOn.rejected]: (state, action) => {
       state.loading = "rejected";
     },
+
+    [getAddOnsList.pending]: (state, action) => {
+      state.loading = "pending";
+    },
+    [getAddOnsList.fulfilled]: (state, action) => {
+      state.loading = "fulfilled";
+      state.addOns = action.payload;
+    },
+    [getAddOnsList.rejected]: (state, action) => {
+      state.loading = "rejected";
+    },
   },
 });
+
+const TEMP_ADD_ONS = [
+  {
+    id: "O7Z6xM6qcUUhY2rRtitG",
+    name: "Jump socks - small (5-9)",
+    photo:
+      "https://firebasestorage.googleapis.com/v0/b/team-sapphire.appspot.com/o/addOns%2Fjump-socks.png?alt=media&token=7a6ef697-ce63-4885-afab-a3134e9d156b",
+    price: "399",
+  },
+
+  {
+    id: "CJaBISha4mFIUrloVFby",
+    name: "Jump socks - medium (10-14)",
+    photo:
+      "https://firebasestorage.googleapis.com/v0/b/team-sapphire.appspot.com/o/addOns%2Fjump-socks.png?alt=media&token=0f02dbdd-0b6a-4bdd-b5c2-f8bf7c1bd26f",
+    price: "399",
+  },
+
+  {
+    id: "z3L1mFxD2UZLdXwGtv3j",
+    name: "Jump socks - large (15+)",
+    photo:
+      "https://firebasestorage.googleapis.com/v0/b/team-sapphire.appspot.com/o/addOns%2Fjump-socks.png?alt=media&token=fe2102cf-cafc-4664-a6a0-f886f0a7f4e2",
+    price: "399",
+  },
+];
 
 export default addOnsSlice.reducer;
