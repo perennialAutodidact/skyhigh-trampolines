df2deacddc4c79657c3f013d33e6f3ab95c2ee69
keegangood <keegood8@gmail.com>
2022-09-30 10:49:44 -0700

    redirect to step 1 if form is reloaded or if an unrecognized path is entered

--- src/components/BookingWizard/index.jsx
+++ src/components/BookingWizard/index.jsx
@@ -1,5 +1,11 @@
 import React, { useReducer } from "react";
-import { Routes, Route } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 import { BookingWizardContext, initialState } from "./context";
 import { wizardReducer } from "./context/reducer";
 import Step1 from "./Step1";
@@ -10,9 +16,19 @@ import Step5 from "./Step5";
 import Step6 from "./Step6";
 import CartPreview from "./common/CartPreview";
 import ProgressBar from "./common/ProgressBar";
+import { useEffect } from "react";
 
 const BookingWizard = () => {
   const [state, dispatch] = useReducer(wizardReducer, initialState);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const pathChunks = location.pathname.split("/");
+    if (pathChunks.length > 2 && state.currentStep === 1) {
+      navigate("/booking");
+    }
+  }, [location, state.currentStep, navigate]);
 
   return (
     <BookingWizardContext.Provider value={[state, dispatch]}>
@@ -29,6 +45,7 @@ const BookingWizard = () => {
                 <Route path="/step-4" element={<Step4 />} />
                 <Route path="/step-5" element={<Step5 />} />
                 <Route path="/checkout" element={<Step6 />} />
+                <Route path="/*" element={<Navigate to="/booking" />} />
               </Routes>
             </div>
           </div>
