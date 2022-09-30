fa634f771aa90a843c8d83c8b87b6e8413755e26
keegangood <keegood8@gmail.com>
2022-09-29 19:37:27 -0700

    created booking in the db when stripe payment intent is created

--- functions/bookings.js
+++ functions/bookings.js
@@ -1,12 +1,15 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
-admin.initializeApp(functions.config().firebase)
-const db = admin.firestore()
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
 
 exports.createBooking = functions.https.onCall((data, context) => {
   functions.logger.log("CREATE BOOKING DATA", data);
-  db.collection("bookings").doc().set({
-    
-  })
+
+  db.collection("bookings")
+    .doc()
+    .set({
+      ...data.metadata,
+    });
 });
