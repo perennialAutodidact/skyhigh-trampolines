4087a5b666168ddc6760b68744d8cd4c95125165
Monique McIntyre <skaiwilliams85@gmail.com>
2022-09-30 08:45:15 -0500

    Updated cloud function to send email as per #26

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,7 @@ const admin = require("firebase-admin");
 const { app } = require("firebase-admin");
 admin.initializeApp();
 const SENDGRID_KEY = functions.config().sendgrid.key;
+const TEMPLATE_ID = functions.config().sendgrid.template;
 sgMail.setApiKey(SENDGRID_KEY);
 
 // firebase function to send email
@@ -15,9 +16,24 @@ exports.sendEmail = functions.https.onCall(async (data, context) => {
   const msg = {
     to: data.to,
     from: "wilado9200@edxplus.com",
-    subject: "Sending with SendGrid is Fun",
-    text: "and easy to do anywhere, even with Node.js",
-    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+    template_id: TEMPLATE_ID,
+
+    dynamic_template_data: {
+      name: data.name,
+      subject: data.subject,
+      idNumber: data.idNumber,
+      currentDate: data.currentDate,
+      orderHistory: data.orderHistory,
+      bookingDate: data.bookingDate,
+      subtotal: data.subtotal,
+      fee: data.fee,
+      tax: data.tax,
+      total: data.total,
+    },
+
+    // subject: "Sending with SendGrid is Fun",
+    // text: "and easy to do anywhere, even with Node.js",
+    // html: "<strong>and easy to do anywhere, even with Node.js</strong>",
   };
   try {
     await sgMail.send(msg);
