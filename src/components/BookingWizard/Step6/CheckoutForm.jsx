c0869e3523557f2721944c144c34aec5dcac8f5b
KG <keegood8@gmail.com>
2022-10-02 10:50:29 -0700

    formatted

--- src/components/BookingWizard/Step6/CheckoutForm.jsx
+++ src/components/BookingWizard/Step6/CheckoutForm.jsx
@@ -1,21 +1,33 @@
 import React from "react";
-import { useElements, useStripe, PaymentElement } from "@stripe/react-stripe-js";
+import {
+  useElements,
+  useStripe,
+  PaymentElement,
+} from "@stripe/react-stripe-js";
 import FormNavButtons from "../common/FormNavButtons";
+import LoadingSpinner from "../../LoadingSpinner";
 const CheckoutForm = ({ goBack }) => {
-  const stripe = useStripe()
+  const stripe = useStripe();
   const elements = useElements();
   const onSubmit = async (e) => {
     e.preventDefault();
 
     const result = await stripe.confirmPayment({
       elements,
-      redirect: 'if_required'
+      redirect: "if_required",
     });
 
     if (result.error) {
       console.log(result.error);
     }
   };
+  //   if (!elements || !stripe) {
+  //     return (
+  //       <div className="my-5">
+  //         <LoadingSpinner />
+  //       </div>
+  //     );
+  //   }
   return (
     <form onSubmit={onSubmit}>
       <PaymentElement />
