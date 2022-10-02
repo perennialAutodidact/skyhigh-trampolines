eccffe83ca8d996822d0c3deb9566d4badf24873
KG <keegood8@gmail.com>
2022-10-01 18:37:34 -0700

    added handleStripeEvent

--- functions/index.js
+++ functions/index.js
@@ -3,14 +3,6 @@ const stripeConfig = require("./stripeConfig");
 const payments = require("./payments");
 const bookings = require("./bookings");
 
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//     response.send({ message: 'Hello form Firebase' })
-// })
-
 exports.createPaymentIntent = payments.createPaymentIntent;
-exports.createBooking = bookings.createBooking;
 exports.handleStripeEvent = stripeConfig.handleStripeEvent;
+exports.writeBookingFromStripeEvent = bookings.writeBookingFromStripeEvent;
--- functions/stripeConfig.js
+++ functions/stripeConfig.js
@@ -1,18 +1,12 @@
 const functions = require("firebase-functions");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const axios = require("axios");
+const admin = require("firebase-admin");
 
-const callCreateBooking = async (data) => {
+if (admin.apps.length === 0) {
+  admin.initializeApp(functions.config().firebase);
+}
 
-  try {
-    return await axios.post(
-      "https://us-central1-team-sapphire.cloudfunctions.net/createBooking",
-      {data}
-    );
-  } catch (error) {
-    throw new functions.https.HttpsError("unknown", JSON.stringify(error.response.data));
-  }
-};
+const db = admin.firestore();
 
 exports.handleStripeEvent = functions.https.onRequest((req, res) => {
   let signature = req.headers["stripe-signature"];
@@ -31,44 +25,9 @@ exports.handleStripeEvent = functions.https.onRequest((req, res) => {
     );
   }
 
-  let paymentIntent;
-  // Handle the event
-  switch (event.type) {
-    case "payment_intent.canceled":
-      paymentIntent = event.data.object;
-      // Then define and call a function to handle the event payment_intent.canceled
-      break;
-    case "payment_intent.created":
-      paymentIntent = event.data.object;
-      functions.logger.log('PAYMENT INTENT', paymentIntent)
-      // Then define and call a function to handle the event payment_intent.created
-      callCreateBooking(paymentIntent);
+  db.collection("stripeEvents")
+    .doc()
+    .set({ ...event });
 
-      break;
-    case "payment_intent.partially_funded":
-      paymentIntent = event.data.object;
-      // Then define and call a function to handle the event payment_intent.partially_funded
-      break;
-    case "payment_intent.payment_failed":
-      paymentIntent = event.data.object;
-      // Then define and call a function to handle the event payment_intent.payment_failed
-      break;
-    case "payment_intent.processing":
-      paymentIntent = event.data.object;
-      // Then define and call a function to handle the event payment_intent.processing
-      break;
-    case "payment_intent.requires_action":
-      paymentIntent = event.data.object;
-      // Then define and call a function to handle the event payment_intent.requires_action
-      break;
-    case "payment_intent.succeeded":
-      paymentIntent = event.data.object;
-      // Then define and call a function to handle the event payment_intent.succeeded
-      break;
-    // ... handle other event types
-    default:
-      console.log(`Unhandled event type ${event.type}`);
-  }
-
-  return res.send({ message: "handleStripeEvent Called" });
+  return res.send();
 });
