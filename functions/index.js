d9e1c94ce58ed716581b20cc4b46e5245617b830
KG <keegood8@gmail.com>
2022-10-02 22:11:46 -0700

    added update paymentt intent

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const payments = require("./payments");
 const bookings = require("./bookings");
 
 exports.createPaymentIntent = payments.createPaymentIntent;
+exports.updatePaymentIntent = payments.updatePaymentIntent;
 exports.cancelPaymentIntent = payments.cancelPaymentIntent;
 exports.handleStripeEvent = stripeConfig.handleStripeEvent;
 exports.writeBookingFromStripeEvent = bookings.writeBookingFromStripeEvent;
