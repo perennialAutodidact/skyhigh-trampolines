475357ec53b7664f154f9c7563b5b5a4aa9aded6
keegangood <keegood8@gmail.com>
2022-10-06 21:21:30 -0700

    moved stripe error to app level state

--- src/components/BookingWizard/Step6/CheckoutForm.jsx
+++ src/components/BookingWizard/Step6/CheckoutForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useMemo } from "react";
+import React, { useContext, useMemo, useState } from "react";
 import {
   useElements,
   useStripe,
@@ -11,10 +11,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { updateBooking } from "../../../redux/bookingsSlice";
 import { BookingWizardContext } from "../context";
 import { getSelectedAddOns } from "../context/utils";
-import { updatePaymentStatus } from "../context/actions";
-import { setStripeLoadingStatus } from "../../../redux/stripeSlice";
+import {
+  setStripeLoadingStatus,
+  setStripeError,
+} from "../../../redux/stripeSlice";
 
-const CheckoutForm = ({ goBack }) => {
+const CheckoutForm = ({ goBack, setError }) => {
   const navigate = useNavigate();
   const stripe = useStripe();
   const elements = useElements();
@@ -22,11 +24,10 @@ const CheckoutForm = ({ goBack }) => {
   const [wizardState, wizardDispatch] = useContext(BookingWizardContext);
   const { addOns, formData } = wizardState;
   const { signatureImageData, fullName, email, address } = formData;
-  const {
-    bookingInProgress: { id: bookingId },
-  } = useSelector((appState) => appState.bookings);
-
-  const { loading: stripeLoadingStatus } = useSelector((state) => state.stripe);
+  const { bookingInProgress } = useSelector((appState) => appState.bookings);
+  const { loading: stripeLoadingStatus, error } = useSelector(
+    (state) => state.stripe
+  );
 
   const bookingData = useMemo(
     () => ({
@@ -55,10 +56,15 @@ const CheckoutForm = ({ goBack }) => {
 
     if (result.error) {
       appDispatch(setStripeLoadingStatus("failed"));
-      console.log(result.error);
+      appDispatch(setStripeError({ error: result.error.message }));
     } else {
       appDispatch(setStripeLoadingStatus("succeeded"));
-      appDispatch(updateBooking({ bookingId, ...bookingData }));
+      appDispatch(
+        updateBooking({
+          bookingId: bookingInProgress?.id,
+          ...bookingData,
+        })
+      );
       navigate("/booking/thank-you");
       // dispatch email thunk
       // redirect to thank you page
