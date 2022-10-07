e8dfb85a358b93596975211eff360c85f7379a7c
keegangood <keegood8@gmail.com>
2022-10-06 21:19:25 -0700

    removed log statement

--- functions/payments.js
+++ functions/payments.js
@@ -1,5 +1,8 @@
 const functions = require("firebase-functions");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const admin = require("firebase-admin");
+
+const db = admin.firestore();
 
 exports.createPaymentIntent = functions.https.onCall(async (data, context) => {
   const { amount, metadata } = data;
@@ -22,7 +25,6 @@ exports.createPaymentIntent = functions.https.onCall(async (data, context) => {
 exports.updatePaymentIntent = functions.https.onCall(
   async (paymentIntentData, context) => {
     const { id, data } = paymentIntentData;
-    functions.logger.log({ id, data });
     try {
       return await stripe.paymentIntents.update(id, { ...data });
     } catch (error) {
