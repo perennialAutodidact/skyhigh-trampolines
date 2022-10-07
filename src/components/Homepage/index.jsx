ef8b7bf4f6ef7dd045d717c89b09a6add7d82334
KG <keegood8@gmail.com>
2022-10-07 10:59:50 -0700

    added useOnLoadImages hook

--- src/components/Homepage/index.jsx
+++ src/components/Homepage/index.jsx
@@ -1,14 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { cancelPaymentIntent } from "../../redux/stripeSlice";
 import { Link } from "react-router-dom";
 import styled from "./Homepage.module.scss";
 import { cancelBooking } from "../../redux/bookingsSlice";
+import { useOnLoadImages } from "../../hooks/useOnLoadImages";
+import LoadingSpinner from "../LoadingSpinner";
 
 const Homepage = () => {
   const appDispatch = useDispatch();
   const { paymentIntent } = useSelector((appState) => appState.stripe);
   const { bookingInProgress } = useSelector((appState) => appState.bookings);
+  const homePageRef = useRef(null);
+  const imagesLoaded = useOnLoadImages(homePageRef);
+
   useEffect(() => {
     if (paymentIntent.id) {
       appDispatch(cancelPaymentIntent(paymentIntent.id));
@@ -19,28 +24,39 @@ const Homepage = () => {
   }, [paymentIntent, bookingInProgress, appDispatch]);
 
   return (
-    <section className={`${styled.homepage} position-relative`}>
-      <figure className="h-100 w-100 position-relative">
-        <img
-          className="img-fluid h-100 w-100"
-          src="https://images.unsplash.com/photo-1612985838143-47ffc9b76532?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1175&q=80"
-          alt="Women jumping in trampoline"
-        />
-      </figure>
+    <div ref={homePageRef}>
+      {!imagesLoaded ? (
+        <div className="vh-100 d-flex justify-content-center align-items-center">
+          <LoadingSpinner />
+        </div>
+      ) : (
+        <section className={`${styled.homepage} position-relative`}>
+          <figure className="h-100 w-100 position-relative">
+            <img
+              className="img-fluid h-100 w-100"
+              src="https://images.unsplash.com/photo-1612985838143-47ffc9b76532?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1175&q=80"
+              alt="Women jumping in trampoline"
+            />
+          </figure>
 
-      <div
-        className={`${styled.overlay} position-absolute d-flex justify-content-center align-items-center w-100 bg-overlay bg-opacity-50`}
-      >
-        <button type="button" className="btn btn-light btn-lg rounded shadow">
-          <Link
-            to="/booking"
-            className="text-decoration-none link-dark fs-5 fw-bold"
+          <div
+            className={`${styled.overlay} position-absolute d-flex justify-content-center align-items-center w-100 bg-overlay bg-opacity-50`}
           >
-            Buy Tickets
-          </Link>
-        </button>
-      </div>
-    </section>
+            <button
+              type="button"
+              className="btn btn-light btn-lg rounded shadow"
+            >
+              <Link
+                to="/booking"
+                className="text-decoration-none link-dark fs-5 fw-bold"
+              >
+                Buy Tickets
+              </Link>
+            </button>
+          </div>
+        </section>
+      )}
+    </div>
   );
 };
 
