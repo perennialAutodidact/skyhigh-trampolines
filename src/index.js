8ec807015de5377cd96bb609a8e30f973f531d2d
KG <keegood8@gmail.com>
2022-10-10 09:24:44 -0700

    added helmet provider and loaded script

--- src/index.js
+++ src/index.js
@@ -6,14 +6,23 @@ import { store } from "./redux/store";
 import "./styles/index.scss";
 import App from "./App";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </Router>
+    <HelmetProvider>
+      <Router>
+        <Provider store={store}>
+          <Helmet>
+            <script
+              src={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_PLACES_API_KEY}&libraries=places`}
+              async
+            ></script>
+          </Helmet>
+          <App />
+        </Provider>
+      </Router>
+    </HelmetProvider>
   </React.StrictMode>
 );
