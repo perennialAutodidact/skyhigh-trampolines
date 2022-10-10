4e5940b43be5308e5bf4567887ecb08be0945871
KG <keegood8@gmail.com>
2022-10-09 22:41:20 -0700

    minor changes

--- src/components/BookingWizard/Step2/index.jsx
+++ src/components/BookingWizard/Step2/index.jsx
@@ -83,7 +83,7 @@ const Step2 = () => {
   };
 
   const goBack = () => {
-    navigate("/booking");
+    navigate("/booking/step-1");
     wizardDispatch(setProgressBarStep(1));
   };
 
@@ -138,7 +138,12 @@ const Step2 = () => {
 
           <Accordion>
             {wizardState.rooms.map((room, index) => (
-              <AccordionItem item={room} headerText={room.name} key={room.id}>
+              <AccordionItem
+                item={room}
+                headerText={room.name}
+                expanded={false}
+                key={room.id}
+              >
                 <AccordionCollapse collapseId={room.id}>
                   <StartTimeList room={room} />
 
--- src/components/BookingWizard/index.jsx
+++ src/components/BookingWizard/index.jsx
@@ -2,9 +2,9 @@ import React, { useReducer, useEffect, useMemo } from "react";
 import {
   Routes,
   Route,
-  Navigate,
   useLocation,
   useNavigate,
+  Navigate,
 } from "react-router-dom";
 import { BookingWizardContext, initialState } from "./context";
 import { wizardReducer } from "./context/reducer";
@@ -18,13 +18,13 @@ import CartPreview from "./common/CartPreview";
 import ProgressBar from "./common/ProgressBar";
 import LoadingSpinner from "../LoadingSpinner";
 import { useStripeClient } from "../../hooks/useStripeClient";
-import ThankYou from "./ThankYou/ThankYou";
+import ThankYou from "./ThankYou";
 
 const BookingWizard = () => {
   const [wizardState, wizardDispatch] = useReducer(wizardReducer, initialState);
   const location = useLocation();
   const navigate = useNavigate();
-  const [stripeClient, loadingStripe] = useStripeClient();
+  const [stripeClient, stripeLoading] = useStripeClient();
 
   const currentPath = useMemo(() => location.pathname.split("/"), [location]);
   const isThankYouPage = useMemo(
@@ -34,14 +34,18 @@ const BookingWizard = () => {
     [currentPath]
   );
 
-  // redirect to step 1 if the page is reloaded and the formData is reset
+  //   redirect to step 1 if the page is reloaded and the formData is reset
   useEffect(() => {
-    if (currentPath.length > 2 && wizardState.currentStep === 1) {
+    if (
+      currentPath.length > 2 &&
+      currentPath[currentPath.length - 1] !== "booking" &&
+      wizardState.currentStep === 1
+    ) {
       navigate("/booking");
     }
   }, [currentPath, wizardState.currentStep, navigate]);
 
-  if (!stripeClient && loadingStripe) {
+  if (!stripeClient && stripeLoading) {
     return (
       <div className="my-5">
         <LoadingSpinner />
@@ -63,15 +67,21 @@ const BookingWizard = () => {
             <div className="border border-grey rounded mt-3">
               <Routes>
                 <Route exact path="/" element={<Step1 />} />
-                <Route path="/step-2" element={<Step2 />} />
-                <Route path="/step-3" element={<Step3 />} />
-                <Route path="/step-4" element={<Step4 />} />
-                <Route path="/step-5" element={<Step5 />} />
+                <Route exact path="/step-2" element={<Step2 />} />
+                <Route exact path="/step-3" element={<Step3 />} />
+                <Route exact path="/step-4" element={<Step4 />} />
+                <Route exact path="/step-5" element={<Step5 />} />
                 <Route
+                  exact
                   path="/checkout"
-                  element={<Step6 stripe={stripeClient} />}
+                  element={
+                    <Step6
+                      stripe={stripeClient}
+                      stripeLoading={stripeLoading}
+                    />
+                  }
                 />
-                <Route path="/thank-you" element={<ThankYou />} />
+                <Route exact path="/thank-you" element={<ThankYou />} />
                 <Route path="/*" element={<Navigate to="/booking" />} />
               </Routes>
             </div>
