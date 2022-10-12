ba07371bb6e01c12fb681734b400b79beb278473
KG <keegood8@gmail.com>
2022-10-11 21:34:57 -0700

    revamped bookings list view

--- src/components/Admin/Bookings/index.jsx
+++ src/components/Admin/Bookings/index.jsx
@@ -2,13 +2,14 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import {
-  getBookingsList,
+  getFirstBookingPage,
   getPrevBookingPage,
   getNextBookingPage,
 } from "../../../redux/bookingsSlice";
 import { getTotalHeadCount } from "../../BookingWizard/context/utils";
 import LoadingSpinner from "../../LoadingSpinner";
 import { useCallback } from "react";
+
 const headerNames = [
   "Booking Date",
   "Receipt #",
@@ -21,33 +22,30 @@ const headerNames = [
 const BookingsList = () => {
   const appDispatch = useDispatch();
   let {
-    bookings,
+    bookingsPage,
     loading: bookingsLoadingStatus,
     page,
     isLastPage,
   } = useSelector((appState) => appState.bookings);
 
-  const nextPage = (newPage) => {
+  const nextPage = () => {
     appDispatch(getNextBookingPage());
   };
 
-  const prevPage = useCallback(
-    (newPage) => {
-      if (page > 1) {
-        appDispatch(getPrevBookingPage());
-      }
-    },
-    [page]
-  );
+  const prevPage = useCallback(() => {
+    if (page > 1) {
+      appDispatch(getPrevBookingPage());
+    }
+  }, [page]);
 
   useEffect(() => {
-    if (!!bookings && bookingsLoadingStatus === "idle") {
-      appDispatch(getBookingsList());
+    if (!!bookingsPage && bookingsLoadingStatus === "idle") {
+      appDispatch(getFirstBookingPage());
     }
-  }, [bookings, bookingsLoadingStatus, appDispatch]);
+  }, [bookingsPage, bookingsLoadingStatus, appDispatch]);
 
   return (
-    <div className="container">
+    <div className="container-fluid">
       <div className="row">
         <div className="col-12">
           <div className="d-flex justify-content-between">
@@ -73,45 +71,47 @@ const BookingsList = () => {
           <LoadingSpinner />
         </div>
       ) : (
-        <table className="table mt-4">
-          <thead>
-            <tr>
-              {headerNames.map((headerName) => (
-                <th>{headerName}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {bookings.map((booking) => (
+        <div className="table-responsive">
+          <table className="table mt-4">
+            <thead>
               <tr>
-                <td>{booking.date}</td>
-                <td>
-                  {booking.receiptId ? (
-                    <Link className="link-dark">{booking.receiptId}</Link>
-                  ) : (
-                    <span className="text-muted">{booking.status}</span>
-                  )}
-                </td>
-                <td>
-                  <div className="d-flex flex-column gap-2">
-                    {booking.rooms.map((room) => (
-                      <div className="d-flex gap-2">
-                        <div className="badge bg-info d-flex align-items-center">
-                          {room.startTime}
-                        </div>
-                        <div>{room.name}</div>
-                      </div>
-                    ))}
-                  </div>
-                </td>
-                <td>{getTotalHeadCount(booking.rooms)}</td>
-                <td>{booking.total ? `$${booking.total / 100}` : "N/A"}</td>
-                <td>{booking.customer?.fullName || "N/A"}</td>
-                <td>{booking.customer?.email || "N/A"}</td>
+                {headerNames.map((headerName) => (
+                  <th>{headerName}</th>
+                ))}
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {bookingsPage.map((booking) => (
+                <tr>
+                  <td>{booking.date}</td>
+                  <td>
+                    {booking.receiptId ? (
+                      <Link className="link-dark">{booking.receiptId}</Link>
+                    ) : (
+                      <span className="text-muted">{booking.status}</span>
+                    )}
+                  </td>
+                  <td>
+                    <div className="d-flex flex-column gap-2">
+                      {booking.rooms.map((room) => (
+                        <div className="d-flex gap-2 align-items-start">
+                          <div className="badge bg-info d-flex align-items-center">
+                            {room.startTime}
+                          </div>
+                          <div>{room.name}</div>
+                        </div>
+                      ))}
+                    </div>
+                  </td>
+                  <td>{getTotalHeadCount(booking.rooms)}</td>
+                  <td>{booking.total ? `$${booking.total / 100}` : "N/A"}</td>
+                  <td>{booking.customer?.fullName || "N/A"}</td>
+                  <td>{booking.customer?.email || "N/A"}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       )}
     </div>
   );
