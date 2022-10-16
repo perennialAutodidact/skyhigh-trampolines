79c4296b90bb3c39ea19e47d5ca0cbe166f8c6a3
Keegan Good <perennialautodidact@gmail.com>
2022-10-16 16:54:25 -0700

    changed thunk condition to cancel thunk only if pending

--- src/redux/utils.js
+++ src/redux/utils.js
@@ -5,8 +5,8 @@ export const createThunkCondition = (sliceName) => ({
 
     const fetchStatus = slice.loading;
     const error = slice.error;
-    if (fetchStatus !== "idle" && fetchStatus !== "succeeded" && !error) {
-      // Already fetched or in progress, don't need to re-fetch
+    if (fetchStatus === "pending") {
+      // Already in progress, don't need to re-fetch
       return false;
     }
   },
