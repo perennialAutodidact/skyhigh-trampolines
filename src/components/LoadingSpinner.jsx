1df66587ac933f7c356b6cb26b3b10cf39cdcde0
Keegan Good <perennialautodidact@gmail.com>
2022-10-16 10:45:37 -0700

    added size prop

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-const LoadingSpinner = ({ color = "primary" }) => {
+const LoadingSpinner = ({ color = "primary", size = null }) => {
   return (
     <div className="d-flex justify-content-center">
-      <div className={`spinner-border text-${color}`} role="status">
+      <div
+        className={`spinner-border text-${color} ${
+          size === "sm" ? "spinner-border-sm my-1" : ""
+        }`}
+        role="status"
+      >
         <span className="visually-hidden">Loading...</span>
       </div>
     </div>
