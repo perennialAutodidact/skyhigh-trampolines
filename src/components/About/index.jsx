46f7ab286d3643a539d3773d7bd475b0f4b7b73a
Keegan Good <perennialautodidact@gmail.com>
2022-10-31 22:05:07 -0700

    setup about page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import AnimatedMask from "./AnimatedMask";
 
 const AboutPage = () => {
-  return <div className="container-fluid"></div>;
+  return (
+    <div className="container-fluid">
+      <div className="row"></div>
+    </div>
+  );
 };
 
 export default AboutPage;
