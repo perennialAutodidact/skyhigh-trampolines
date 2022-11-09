08affb41dfcdeb0a069e4bdf7daf3c27041966c8
keegangood <keegood8@gmail.com>
2022-11-08 16:18:22 -0800

    moved repeating values to maps

--- src/components/About/IntroSection/index.jsx
+++ src/components/About/IntroSection/index.jsx
@@ -3,9 +3,7 @@ import GithubDev from "./GithubDev";
 
 const IntroSection = () => {
   return (
-    <div
-      className="container-fluid py-3 py-lg-5 vh-75 overflow-hidden"
-    >
+    <div className="container-fluid py-3 py-lg-5 vh-75 overflow-hidden">
       <div className="row gy-2 gy-lg-3 g-xl-2">
         <div className="col-12 col-md-8 ps-md-5">
           <p id="p1" className="fs-3 fs-lg-2">
@@ -26,30 +24,11 @@ const IntroSection = () => {
         <div className="col-12 col-md-10 offset-md-1 bg-info p-5">
           <div className="container">
             <div className="row gy-5">
-              <div className="col-6 col-md-3">
-                <GithubDev
-                  name={"Keegan Good"}
-                  url={"https://github.com/perennialautodidact"}
-                />
-              </div>
-              <div className="col-6 col-md-3 d-flex flex-column align-items-center">
-                <GithubDev
-                  name={"Monique"}
-                  url={"https://github.com/m-oniqu3"}
-                />
-              </div>
-              <div className="col-6 col-md-3 d-flex flex-column align-items-center">
-                <GithubDev
-                  name={"Jin Choo"}
-                  url={"https://github.com/jinchoo"}
-                />
-              </div>
-              <div className="col-6 col-md-3 d-flex flex-column align-items-center">
-                <GithubDev
-                  name={"waldothedeveloper"}
-                  url={"https://github.com/waldothedeveloper"}
-                />
-              </div>
+              {DEVS.map(({ name, url }) => (
+                <div className="col-6 col-md-3" key={name}>
+                  <GithubDev name={name} url={url} />
+                </div>
+              ))}
             </div>
           </div>
         </div>
@@ -57,5 +36,24 @@ const IntroSection = () => {
     </div>
   );
 };
+
+const DEVS = [
+  {
+    name: "Keegan Good",
+    url: "https://github.com/perennialautodidact",
+  },
+  {
+    name: "Monique",
+    url: "https://github.com/m-oniq3",
+  },
+  {
+    name: "Jin Choo",
+    url: "https://github.com/jinchoo",
+  },
+  {
+    name: "Waldo",
+    url: "https://github.com/waldothedeveloper",
+  },
+];
 
 export default IntroSection;
