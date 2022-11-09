c875e29bf2e5ac50b96bb190382bd2bc7b39a676
keegangood <keegood8@gmail.com>
2022-11-09 14:38:42 -0800

    moved NumberCircle inside ScreenshotList folder

--- src/components/About/ScreenshotList/NumberCircle/index.jsx
+++ src/components/About/ScreenshotList/NumberCircle/index.jsx
@@ -4,7 +4,7 @@ import styles from "./NumberCircle.module.scss";
 const NumberCircle = ({ number }) => {
   return (
     <div
-      id={`number-circle-${number}`}
+      id={`number-circle`}
       style={{ visibility: "hidden" }}
       className={`
         bg-info text-light rounded-circle 
