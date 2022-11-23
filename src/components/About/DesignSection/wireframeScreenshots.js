72b02975f6c83a583e18f1fd45f11f604f958fbd
keegangood <keegood8@gmail.com>
2022-11-22 16:00:20 -0800

    added design section scroll animations

---
+++ src/components/About/DesignSection/wireframeScreenshots.js
@@ -0,0 +1,54 @@
+export const wireframeScreenshots = [
+  {
+    caption: {
+      header: "Homepage",
+      text: "The homepage was left up to the imagination.",
+    },
+    fileName: "homepage.jpeg",
+  },
+  {
+    caption: { header: "Booking Wizard", text: "Step 1" },
+    fileName: "booking-wizard-step1.jpeg",
+  },
+  {
+    caption: { header: "Booking Wizard", text: "Step 2" },
+    fileName: "booking-wizard-step2.jpeg",
+  },
+  {
+    caption: { header: "Booking Wizard", text: "Step 3" },
+    fileName: "booking-wizard-step3.jpeg",
+  },
+  {
+    caption: { header: "Booking Wizard", text: "Step 4" },
+    fileName: "booking-wizard-step4.jpeg",
+  },
+  {
+    caption: { header: "Booking Wizard", text: "Step 5" },
+    fileName: "booking-wizard-step5.jpeg",
+  },
+  {
+    caption: { header: "Booking Wizard", text: "Step 6" },
+    fileName: "booking-wizard-step6.jpeg",
+  },
+  {
+    caption: {
+      header: "Admin Bookings List",
+      text: "List view for all bookings",
+    },
+    fileName: "admin-booking-list.jpeg",
+  },
+  {
+    caption: {
+      header: "Admin Booking Detail",
+      text: "Detail view for a single booking",
+    },
+    fileName: "admin-booking-detail.jpeg",
+  },
+  {
+    caption: {
+      header: "Daily Availability",
+      text: "The design for this page was augmented to meet changes to the app's functionality.",
+    },
+    fileName: "admin-daily-availability.jpeg",
+  },
+];
